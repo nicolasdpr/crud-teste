bb3bcebc1891e2a33dd78f2fa2ea600b6a80304e
nicolasdpr <30910348+nicolasdpr@users.noreply.github.com>
2024-11-02 20:39:49 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'teste');
+    define('PASS', 'teste');
+    define('BASE', 'cadastro');
+
+    $conn = new MySQLi(HOST,USER,PASS,BASE);
---
+++ salvar_usuario.php
@@ -0,0 +1,78 @@
+<?php
+
+if (!$conn) {
+     die("Erro na conexão: " . mysqli_connect_error());}
+
+
+
+     
+    switch($_REQUEST["acao"]) {
+        case 'cadastrar':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+            $data_nasc = $_POST["data_nasc"];
+
+            $sql = "INSERT INTO usuarios (nome, email, senha, data_nasc) 
+            VALUES ('{$nome}', '{$email}', '{$senha}', '{$data_nasc}')";
+
+            $res = $conn->query($sql);
+
+            if($res==true){
+                print "<script>alert('Cadastro feito com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else{
+                print "<script>alert('Não foi possível cadastrar');</script>";
+                print "<script>location.href='?page=listar';</script>";
+
+            }
+            break;
+
+        case 'editar':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+            $data_nasc = $_POST["data_nasc"];
+
+            $sql = "UPDATE usuarios SET
+                        nome='{$nome}',
+                        email='{$email}',
+                        senha='{$senha}',
+                        data_nasc='{$data_nasc}'
+                    WHERE 
+                        id=".$_REQUEST["id"];
+                        
+
+            $res = $conn->query($sql);
+
+            if($res==true){
+                print "<script>alert('Cadastro editado com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else{
+                print "<script>alert('Não foi possível editar cadastro');</script>";
+                print "<script>location.href='?page=listar';</script>";
+
+            }
+            
+
+            break;
+
+        case 'excluir':
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+
+            $res = $conn->query($sql);
+
+
+            if($res==true){
+                print "<script>alert('Cadastro excluído com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else{
+                print "<script>alert('Não foi possível excluir cadastro');</script>";
+                print "<script>location.href='?page=listar';</script>";
+
+
+            }
+            break;
+        }
+
+       
